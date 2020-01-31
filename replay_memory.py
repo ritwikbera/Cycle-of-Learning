d9eadaed01a6ffb297b087ef1f0236df5c280c06
Ritwik Bera <ritwikbera12@gmail.com>
2020-01-31 15:54:05 -0600

    added ddpg

--- replay_memory.py
+++ replay_memory.py
@@ -1,30 +1,29 @@
-from collections import namedtuple
 import random
+from collections import namedtuple
 
 # Taken from
 # https://github.com/pytorch/tutorials/blob/master/Reinforcement%20(Q-)Learning%20with%20PyTorch.ipynb
 
-Transition = namedtuple('Transition', ('state', 'action', 'mask', 'next_state',
-                                       'reward'))
+Transition = namedtuple(
+    'Transition', ('state', 'action', 'mask', 'next_state', 'reward'))
 
 
-class Memory(object):
-    def __init__(self):
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
         self.memory = []
+        self.position = 0
 
     def push(self, *args):
         """Saves a transition."""
-        self.memory.append(Transition(*args))
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
 
-    def sample(self, batch_size=None):
-        if batch_size is None:
-            return Transition(*zip(*self.memory))
-        else:
-            random_batch = random.sample(self.memory, batch_size)
-            return Transition(*zip(*random_batch))
-
-    def append(self, new_memory):
-        self.memory += new_memory.memory
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
 
     def __len__(self):
         return len(self.memory)
